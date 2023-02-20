ea4b66f2d1774b282ab00a39a1fda8fda158b040
Victor Laguna <cvx.vlaguna@gmail.com>
2023-02-20 18:04:19 -0300

    mejoras de menu, trabajo en la carga a base

--- mail.py
+++ mail.py
@@ -3,13 +3,27 @@ import smtplib
 from email.message import EmailMessage
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from pathlib import Path
 import logging
+import MySQLdb
 import mysql.connector as msql
 from mysql.connector import Error
 import csv
+from os import system, name
 import time
 import traceback
 import ssl
+
+# define our clear function
+def clear():
+ 
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+ 
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
 # basic configuration for application logs
 logging.basicConfig(filename='mailsender.log', filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
@@ -20,16 +34,17 @@ def connectdb(host,user,password):
     # example conn = msql.connect(host='192.168.1.3', user='root', password='Vick-2016')
     conn = msql.connect(host=host, user=user, password=password)
     try:
-        if conn.is_connected() == True:
-            logging.info('DB connected success')
-            return(print("Connection to DB connected success"))
-        else:
-                logging.error('DB no connected')
-                return(print('DB no connected'))
-    except mysql.connector.Error as error:
-        print("Failed to insert record into Laptop table {}".format(error))
-        logging.warning('DB no connected, please verify inputs')
-        print("cannot connect to the DB")
+        conn
+    except MySQLdb.connector.Error as error:
+            logging.warning('DB no connected, please verify inputs')
+            print("cannot connect to the DB")
+            logging.error('DB no connected')
+            return(print('DB no connected'))
+    else:
+        logging.info('DB connected success')
+        print('\nDB connected success')
+        response = conn.is_connected()
+        return response
 
 # Funtion to send email one by one
 def sendmail(asunto,destinatario,destino,password,email_smtp):
@@ -64,21 +79,28 @@ def sendmail(asunto,destinatario,destino,password,email_smtp):
     return(response)
 
 # Funtion to put into the mysql database all the mail from a csv list
-def load_mail_list(file_path):
-    logging.info('DB connection init')
+def load_mail_list(host,user,password,file_path):
     # example conn = msql.connect(host='192.168.1.3', user='root', password='Vick-2016')
     #conn = msql.connect(host, user, password)
+    conn = msql.connect(host=host, user=user, password=password)
     try:
-        if conn.is_connected() == True:
-            logging.info('DB connected success')
-            cursor = conn.cursor()
-            cursor.execute("CREATE DATABASE IF NOT EXISTS mailsender")
-            conn.database = 'mailsender'
-            cursor.execute('CREATE TABLE IF NOT EXISTS `mail_list` (`id` INT(12) NOT NULL AUTO_INCREMENT, `nombre` VARCHAR(50), `mail` VARCHAR(50), `accountid` INT(12),`historyid` INT(12),`created` DATE, PRIMARY KEY (id))')
-            #get_list_fromdb = list(cursor.execute("SELECT * FROM `mailsender`.`mail_list`;"))
+        conn
+    except NameError:
+        logging.error('DB no connected')
+        print('DB no connected')
+    else:
+        logging.info('DB connection init')
+        logging.info('DB connected success')
+        cursor = conn.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS mailsender")
+        conn.database = 'mailsender'
+        cursor.execute('CREATE TABLE IF NOT EXISTS `mail_list` (`id` INT(12) NOT NULL AUTO_INCREMENT, `nombre` VARCHAR(50), `mail` VARCHAR(50), `accountid` INT(12),`historyid` INT(12),`created` DATE, PRIMARY KEY (id))')
+        #get_list_fromdb = list(cursor.execute("SELECT * FROM `mailsender`.`mail_list`;"))
+        my_file = Path(file_path)
+        if my_file.is_dir() and my_file.is_file():
             with open(file_path, newline='') as f:
                 reader = csv.reader(f)
-                read_list_frompath = list(reader)
+            read_list_frompath = list(reader)
             account_list = [fila[2] for fila in read_list_frompath]    
             mails_list = [fila[1] for fila in read_list_frompath]
             name_list = [fila[0] for fila in read_list_frompath]
@@ -90,47 +112,57 @@ def load_mail_list(file_path):
             logging.info('Record inserted successfully into Laptop table')
             conn.close()
         else:
-            logging.error('DB no connected')
-            print('DB no connected')
-    except mysql.connector.Error as error:
-        print("Failed to insert record into Laptop table {}".format(error))
-        logging.warning('DB no connected, please verify inputs')
-        print("cannot connect to the DB")
-
+            print("\nFile not exists")
+            logging.info('File not exists')
+   
 # ejecucion principal
 if __name__ == '__main__':
-    print("welcome to the mailsender menu, please select the requeriment:")
+    clear()
+    print("\nWelcome to the mailsender menu, please select the requeriment:")
     ans=True
+    response=False
     while ans:
         print ("""
         1.Connect with Database connection
         2.Verify DB connection
-        2.Upload to database the mail list
-        3.Send all the pending mail for this account
-        4.Exit/Quit
+        3.Upload to database the CSV mail list
+        4.Configure path for mail html template
+        5.Send all the pending mail for this account
+        6.Exit/Quit
         """)
         ans=str(input("What would you like to do? ")) 
         if ans=="1": 
             print("\n Please set this ")
-            host = str(input ("please input the host for the mysql database: "))
-            user = str(input ("please input the user for the mysql database: "))
-            password = str(input ("please input the password for the mysql database: "))
-            print(host, user, password)
-            connectdb(host,user,password)
+            host = str(input ("  please input the host for the mysql database: "))
+            user = str(input ("  please input the user for the mysql database: "))
+            password = str(input ("  please input the password for the mysql database: "))
+            #print(host, user, password)
+            response = connectdb(host,user,password)
         elif ans=="2":
-            if conn.is_connected() == True or conn in vars():
+            #print(response)
+            if response == True:
                 logging.info('DB connected success')
-                print("Connection to DB connected success")
+                print("\nConnection to DB connected success")
             else:
                 logging.error('DB no connected')
-                print('DB no connected')
+                print('\nDB no connected')
         elif ans=="3":
-            print("\n Upload mail list selected")
-            path = str(input ("please input the path csv list on the host"))
-            load_mail_list()
+            if response == True:
+                #print(host,user,password,response)
+                file_path = str(input ("please input the path csv list on the host: "))
+                print(file_path)
+                load_mail_list(host,user,password,file_path)
+            else:
+                print('\nDB no connected, please select the option 1 and do the connection')
         elif ans=="4":
-            print("\n Student Record Found") 
+            print("\nClosing DB connection")
+            if response == True:
+                conn.close()
+            else:
+                print("DB not connected is not necessary close")
         elif ans=="5":
+            print("\n Student Record Found") 
+        elif ans=="6":
             print("\n Goodbye")
             exit() 
         elif ans !="":
